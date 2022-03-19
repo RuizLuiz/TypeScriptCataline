4b26ceec3f961b92036c210c8508e5b9bacbcb99
Luiz Ruiz <luizruiz04@gmail.com>
2022-03-18 21:57:25 -0400

    TypeScript types
    array, tuple, enum, union, aliases, Inference, Assertion

--- src/main.ts
+++ src/main.ts
@@ -47,3 +47,76 @@ showInformation()
 function error(): never{
     throw new Error('error')
 }
+
+// array 
+
+let items: Array<number>
+
+items = [1, 2, 3, 4]
+
+console.log(items)
+
+let items2: string[]
+
+items2 = ['luiz','ruiz']
+
+console.log(items2)
+
+//tuple
+
+let items3: [number, string, boolean]
+
+items3 = [1, 'luiz', true]
+
+console.log(items3)
+
+//enum
+
+enum Colors {
+    white = '#fff',
+    black = '#000',
+}
+
+let white: Colors = Colors.white
+
+console.log(white)
+
+//type union 
+
+let age: number | string
+
+age = 20
+
+age = 'vinte'
+
+console.log(age)
+
+function showPet(pet: 'dog' | 'cat' | 'zebra'){
+    console.log(pet)
+}
+
+showPet('cat')
+
+//type aliases
+
+type Pet = 'dog' | 'cat' | 'zebra'
+
+function displayPet(pet: Pet){
+    console.log(pet)
+}
+
+displayPet('zebra')
+
+//type Inference
+
+let message2 = 'Cataline' // infere o tipo a partir da variável atribuída
+                          // infers the type depending on the variable inserted                  
+// window.addEventListener('click', (event) =>{
+//     console.log(event.target)
+// })
+
+//type Assertion
+const inputName = document.querySelector('#name') // if query  return null, variable null
+const inputName2 = document.querySelector('#name') as HTMLInputElement 
+
+inputName2.value
